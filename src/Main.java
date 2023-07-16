9bad372f4b29261018de72d78da31142f772421c
Pepi100 <radunedelcu02@gmail.com>
2023-07-16 19:23:30 +0300

    Image Negative

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import javax.imageio.ImageIO;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(22);
+        try {
+            FileInputStream fin = new FileInputStream("test/baboon.bmp");
+            FileOutputStream fout = new FileOutputStream("test/complement_baboon.bmp");
+            byte[] header = new byte[54];
+            fin.read(header);
+            fout.write(header);
+
+            int octet;
+            while((octet = fin.read()) != -1)
+                fout.write(255 - octet);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
